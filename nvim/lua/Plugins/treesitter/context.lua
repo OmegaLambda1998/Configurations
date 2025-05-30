49d64de9c0323eb43e98d57c6a46c36d2914884b
Patrick Armstrong <patrick.james.1998@gmail.com>
2025-05-30 12:18:08 +1000

    Update treesitter-context

--- nvim/lua/Plugins/treesitter/context.lua
+++ nvim/lua/Plugins/treesitter/context.lua
@@ -5,19 +5,9 @@ local url = "nvim-treesitter/nvim-treesitter-context"
 local context = CFG.spec:add(url)
 context.main = "treesitter-context"
 
----@type TSContext.Config
+---@type TSContext.UserConfig
 context.opts = {
     enable = true,
-    multiwindow = false,
-    max_lines = 0,
-    min_window_height = 0,
-    line_numbers = true,
-    multiline_threshold = 20,
-    trim_scope = "outer",
-    mode = "cursor",
-    separator = nil,
-    zindex = 20,
-    on_attach = nil,
 }
 
 CFG.colourscheme:set("treesitter_context")
